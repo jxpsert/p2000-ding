b8566785b8920a89b3f926c75404db36939c2c4c
jxpsert <jplatenburg@outlook.com>
2024-01-24 09:21:44 +0100

    Added case sensitivity to filters

--- feed.js
+++ feed.js
@@ -1,23 +1,23 @@
-const RSFE = require('rss-feed-emitter');
+const RSFE = require("rss-feed-emitter");
 const feed = new RSFE({ skipFirstLoad: true });
 
 feed.add({
-    //url: "http://p2000.brandweer-berkel-enschot.nl/rss.asp",
-    url: "https://112-nu.nl/hulpdiensten/rss",
-    refresh: 2000,
-    eventName: "new-p2000"
-})
+  //url: "http://p2000.brandweer-berkel-enschot.nl/rss.asp",
+  url: "https://112-nu.nl/hulpdiensten/rss",
+  refresh: 2000,
+  eventName: "new-p2000",
+});
 
 function checkFilters(filters, message) {
-    return filters.some(filter=>message.toLowerCase().includes(filter));
+  return filters.some((filter) => message.includes(filter));
 }
 
 function isBCall(message) {
-    let b = ["b2", "b1"];
-    return b.some(call=>message.toLowerCase().includes(call));
+  let b = ["b2", "b1"];
+  return b.some((call) => message.toLowerCase().includes(call));
 }
 
-feed.on('error', console.error);
+feed.on("error", console.error);
 
 exports.feed = feed;
 exports.checkP2KFilter = checkFilters;
